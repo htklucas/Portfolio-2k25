3e23c954a12750b4ffed81c7d6107b86c0e4b086
Lucas Josué Maia <161860612+htklucas@users.noreply.github.com>
2025-04-22 08:52:06 -0300

    Delete home directory

--- home/index.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function() {
-    const header = document.querySelector('header');
-    header.classList.toggle('scrolled', window.scrollY > 50);
-});
-
-function scrollToTop() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  }
